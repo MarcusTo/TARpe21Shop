b21e88c921d17c40a183aac830934af4f2f22abb
Opilane TTHK <opilane@tthk.ee>
2023-10-30 13:45:44 +0200

    Migrate database, add view to display index page

---
+++ TARpe21ShopVaitmaa.Core/ServiceInterface/IRealEstatesServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TARpe21ShopVaitmaa.Core.Domain;
+using TARpe21ShopVaitmaa.Core.Dto;
+
+namespace TARpe21ShopVaitmaa.Core.ServiceInterface
+{
+    public interface IRealEstatesServices
+    {
+        Task<RealEstate> GetAsync();
+        Task<RealEstate> Create(RealEstateDto dto);
+    }
+}
---
+++ TARpe21ShopVaitmaa/Controllers/RealEstatesControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TARpe21ShopVaitmaa.Models.Spaceship
+{
+    public class RealEstatesControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
